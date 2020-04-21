cc72d681bfb2ba1724067f7e7662941d43dca525
Evan Burton <evan@heyevan.com>
2020-04-21 07:58:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
   monopoly = {:railroads =>
     {
       :pieces=> 4,
-      :rent_in_dollars=> {
+      :rent_in_dollars=> {}
       :names=> {},
       :one_piece_owned =>25,
       :two_pieces_owned=>50,
